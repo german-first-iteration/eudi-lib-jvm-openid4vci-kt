35f8334dccbf4da19691ee9526b7e7b110ac394c
Tobias Thierer <tobias.thierer@eudi.sprind.org>
2025-12-09 22:06:23 +0100

    [WD-2049] build.gradle.kts: Modify to be able to build as part of android-wallet-app project.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
-import com.vanniktech.maven.publish.JavadocJar
-import com.vanniktech.maven.publish.KotlinJvm
-import org.jetbrains.dokka.gradle.engine.parameters.VisibilityModifier
+//import com.vanniktech.maven.publish.JavadocJar
+//import com.vanniktech.maven.publish.KotlinJvm
+//import org.jetbrains.dokka.gradle.engine.parameters.VisibilityModifier
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
-import java.net.URI
+//import org.owasp.dependencycheck.gradle.extension.DependencyCheckExtension
+import java.net.URL
 
 object Meta {
     const val BASE_URL = "https://github.com/eu-digital-identity-wallet/eudi-lib-jvm-openid4vci-kt"
@@ -11,13 +12,18 @@ object Meta {
 plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlin.serialization)
+    // EUDI-removed
+    /*
     alias(libs.plugins.spotless)
     alias(libs.plugins.kover)
     alias(libs.plugins.dokka)
     alias(libs.plugins.maven.publish)
     alias(libs.plugins.dependency.check)
+    */
 }
 
+// EUDI-removed
+/*
 repositories {
     mavenCentral()
     mavenLocal()
@@ -26,6 +32,7 @@ repositories {
         mavenContent { snapshotsOnly() }
     }
 }
+*/
 
 dependencies {
     api(libs.nimbus.oauth2.oidc.sdk)
@@ -35,17 +42,25 @@ dependencies {
     api(libs.kotlinx.serialization.json)
     api(libs.ktor.serialization.kotlinx.json)
     testImplementation(libs.kotlinx.coroutines.test)
+    // EUDI-removed
+    /*
     testImplementation(libs.jsoup)
+    */
     testImplementation(kotlin("test"))
+    // EUDI-removed
+    /*
     testImplementation(libs.ktor.client.okhttp)
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.ktor.server.content.negotiation)
     testImplementation(libs.ktor.client.mock)
+     */
     testImplementation(libs.ktor.client.logging)
+    // EUDI-removed
+    /*
     testImplementation(libs.logback.classic)
     testImplementation(libs.cbor)
     testImplementation(libs.tink)
-    testImplementation(libs.bouncycastle.pkix)
+    */
 }
 
 java {
@@ -62,6 +77,8 @@ kotlin {
     }
 }
 
+// EUDI-removed
+/*
 spotless {
     kotlin {
         ktlint(libs.versions.ktlint.get())
@@ -129,3 +146,4 @@ dependencyCheck {
         apiKey = System.getenv("NVD_API_KEY") ?: properties["nvdApiKey"]?.toString() ?: ""
     }
 }
+*/
