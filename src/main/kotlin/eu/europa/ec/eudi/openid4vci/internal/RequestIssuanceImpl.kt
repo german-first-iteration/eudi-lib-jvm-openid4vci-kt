918053623fcd8dc42b7cbffcdb4e28101f4117e4
Tobias Thierer <tobias.thierer@eudi.sprind.org>
2026-07-20 21:02:30 +0200

    Treat key_attestations_required as optional when parsing proof types
    https://openid.net/specs/openid-4-verifiable-credential-issuance-1_0.html
    says: *`key_attestations_required`: OPTIONAL*.
    
    Further, absent is different from empty (both are allowed): *If the Credential Issuer does not require a key attestation, this parameter MUST NOT be present in the metadata. If both key_storage and user_authentication parameters are absent, the key_attestations_required parameter may be empty, indicating a key attestation is needed without additional constraints.*
    
    The metadata parser in this library previously (wrongly) asserted that key_attestations_required is present.
    
    This commit fixes this.

--- src/main/kotlin/eu/europa/ec/eudi/openid4vci/internal/RequestIssuanceImpl.kt
+++ src/main/kotlin/eu/europa/ec/eudi/openid4vci/internal/RequestIssuanceImpl.kt
@@ -234,7 +234,7 @@ internal class RequestIssuanceImpl(
     ): Proof.Jwt {
         val proofSigner = proofSpecification.proofSignerProvider(
             cNonce,
-            proofRequirement.keyAttestationRequirement.preferredKeyStorageStatusPeriod,
+            proofRequirement.keyAttestationRequirement?.preferredKeyStorageStatusPeriod,
         )
         val joseAlg = run {
             val javaSigningAlgorithm = proofSigner.javaAlgorithm
@@ -269,7 +269,7 @@ internal class RequestIssuanceImpl(
     ): Proof.Attestation {
         val keyAttestationJwt = proofSpecification.attestationProvider(
             cNonce,
-            proofRequirement.keyAttestationRequirement.preferredKeyStorageStatusPeriod,
+            proofRequirement.keyAttestationRequirement?.preferredKeyStorageStatusPeriod,
         )
         keyAttestationJwt.ensureKeyAttestationJwtAlgIsSupported(proofRequirement)
         keyAttestationJwt.attestedKeys.assertMatchesBatchIssuanceBatchSize(selectedReusePolicy)
